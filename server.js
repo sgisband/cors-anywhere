0688b0d2212b665973edd3fda31449b44335ab9f
sgisband <122428506+sgisband@users.noreply.github.com>
2023-02-28 14:25:52 +0900

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   // originWhitelist: originWhitelist,  
-  originWhitelist: ['http://localhost:8080', 'http://220.123.99.82:8080'],  
+  originWhitelist: ['http://localhost:8080', 'http://220.123.99.82:8048'],  
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
